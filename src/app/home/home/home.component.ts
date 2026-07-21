d877173ca56e7a246fcc3b23bb53c44f53f81690
Sunil kumar <kumarsuneel162@gmail.com>
2026-07-21 15:40:08 +0530

    adding the array

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  users = [
+    {
+      id: 101,
+      name: "Sunil",
+    },
+    {
+      id: 102,
+      name: "Rahul",
+    },
+    {
+      id: 103,
+      name: "Admin",
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

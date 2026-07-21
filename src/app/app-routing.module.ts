ffda31a1619758de8545b6fbb82a5259d4a179a5
Sunil kumar <kumarsuneel162@gmail.com>
2026-07-21 14:45:07 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", loadChildren: "./home/home.module#HomeModule" },
+  { path: "admin", loadChildren: "./admin/admin.module#AdminModule" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
